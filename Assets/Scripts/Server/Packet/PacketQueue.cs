a118f975bbb9e67859ca84c4e17a4c77a7762a30
정성원 <tyhg56@naver.com>
2023-02-05 18:28:01 +0900

    Feat : " PacketQueue 추가 "
    PacketQueue 추가
     - 서버에서 온 네트워크 패킷을 담아둘 Que

---
+++ Assets/Scripts/Server/Packet/PacketQueue.cs
@@ -0,0 +1,57 @@
+﻿using ServerCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPacketMessage
+{
+    // 처리할 패킷
+    public CMessage Message { get; set; }
+    // 패킷을 처리할 함수
+    public Action<CMessage> UnPackMessageFunc { get; set; }
+}
+
+public class CPacketQueue
+{
+    public static CPacketQueue GetInstance { get; } = new CPacketQueue();
+
+    Queue<CPacketMessage> _packetQueue = new Queue<CPacketMessage>();
+    object _lock = new object();
+
+    public void Push(CMessage Packet, Action<CMessage> action)
+    {
+        lock (_lock)
+        {
+            CPacketMessage PacketMessage = new CPacketMessage();     
+            PacketMessage.Message = Packet;
+            PacketMessage.UnPackMessageFunc = action;
+
+            _packetQueue.Enqueue(PacketMessage);            
+        }        
+    }
+
+    public CPacketMessage Pop()
+    {
+        lock (_lock)
+        {
+            if (_packetQueue.Count == 0)
+                return null;
+
+            return _packetQueue.Dequeue();
+        }
+    }
+
+    public List<CPacketMessage> PopAll()
+    {
+        List<CPacketMessage> list = new List<CPacketMessage>();
+
+        lock (_lock)
+        {
+            while (_packetQueue.Count > 0)
+                list.Add(_packetQueue.Dequeue());
+        }
+
+        return list;
+    } 
+}
