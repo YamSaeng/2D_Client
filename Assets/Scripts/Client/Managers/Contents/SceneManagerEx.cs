80d734868e852f641c69ae28d8ea7ed0c626e851
정성원 <tyhg56@naver.com>
2023-02-06 21:56:44 +0900

    Feat : " SceneManager 추가 "
    SceneManager 추가 ( Scene 관리 )

---
+++ Assets/Scripts/Client/Managers/Contents/SceneManagerEx.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerEx
+{
+    public BaseScene CurrentScene
+    {
+        get
+        {
+            return GameObject.FindObjectOfType<BaseScene>();
+        }
+    }
+
+    public void LoadScene(Define.en_Scene SceneType)
+    {
+        Managers.Clear();        
+        SceneManager.LoadScene(GetSceneName(SceneType));
+    }
+
+    string GetSceneName(Define.en_Scene SceneType)
+    {
+        string SceneName = System.Enum.GetName(typeof(Define.en_Scene), SceneType);
+        return SceneName;
+    }
+
+    public void Clear()
+    {
+        CurrentScene.Clear();
+    }
+}
