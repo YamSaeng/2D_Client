a9e93bf1362558a760eaa8ad004dd8d62e1101fd
정성원 <tyhg56@naver.com>
2023-02-06 14:16:18 +0900

    Feat : " BaseScene 추가 "
    BaseScene 추가
     - UnityScene을 관리해줄 클래스

---
+++ Assets/Scripts/Client/Scene/BaseScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public abstract class BaseScene : MonoBehaviour
+{
+    //스크립트를 붙인 GameObject가 활성화가 되어 있어야 Start에 진입한다.
+    //비활성화가 되잇을때의 Start역할을 하는 함수는 Awake()함수이다.
+
+    public Define.en_Scene _SceneType
+    {
+        get;
+        protected set;
+    } = Define.en_Scene.None;
+
+    void Awake()
+    {
+        Init();
+    }
+
+    protected virtual void Init()
+    {
+        Object EventSystem = GameObject.FindObjectOfType(typeof(EventSystem));
+        if(EventSystem == null)
+        {
+            Managers.Resource.Instantiate(en_ResourceName.CLIENT_UI_EVENT_SYTEM).name = "@EventSystem";
+        }
+    }
+
+    public abstract void Clear();
+}
