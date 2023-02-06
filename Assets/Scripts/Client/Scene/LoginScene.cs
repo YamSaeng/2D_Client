2149327ead14c368e639480ea3e9851f3d5a07ee
정성원 <tyhg56@naver.com>
2023-02-06 16:53:14 +0900

    Feat : " LoginScene 추가 "
    LoginScene 추가 ( 로그인 씬 )

---
+++ Assets/Scripts/Client/Scene/LoginScene.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class LoginScene : BaseScene
+{
+    UI_LoginScene _LoginSceneUI;
+
+    protected override void Init()
+    {
+        base.Init();
+
+        Managers.Sound.PlayBGM(en_SoundClip.SOUND_CLIP_LOGIN, 0.1f);
+
+        _SceneType = Define.en_Scene.LoginScene;
+                
+        //게임 화면 크기 640 480으로 조정
+        Screen.SetResolution(800, 600, false);
+
+        _LoginSceneUI = Managers.UI.ShowSceneUI<UI_LoginScene>(en_ResourceName.CLIENT_UI_SCENE_LOGIN);        
+    }
+
+    public override void Clear()
+    {
+
+    }
+}
