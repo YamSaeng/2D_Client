d8c0e759dc57a9370c640963a39bfbbe6e8307b0
정성원 <tyhg56@naver.com>
2023-02-08 08:58:08 +0900

    Feat : " 채팅 UI 추가 "
    UI_ChattingBoxGroup ( 채팅 박스 그룹 UI ) 추가
    
    UI_ChattingBox ( 채팅 박스 UI ) 추가

---
+++ Assets/Scripts/Client/UI/ChattingBox/UI_ChattingBox.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+//------------------------------------
+// 채팅 박스 UI
+//------------------------------------
+public class UI_ChattingBox : UI_Scene
+{
+    public int _ChattingBoxIndex;
+    // 표시할 채팅 메세지 최대 개수
+    int MaxMessage = 30;
+
+    // 채팅 메세지
+    public class CChattingMessage
+    {
+        public TextMeshProUGUI ChattingTextObject;             
+    }   
+
+    // 내부적으로 보관할 채팅메세지 배열
+    List<CChattingMessage> ChattingMessageList;    
+        
+    enum en_ChattingBoxGameObject
+    {
+        ChattingPannel, // 생성한 채팅메세지를 자식으로 붙일 패널
+        ChattingBox,
+        ChattingViewSubject
+    }
+
+    enum en_ChattingBoxButton
+    {
+        ChattingSubjectButton
+    }
+
+    enum en_ChattingBoxText
+    {
+        ChattingSubjectText
+    }
+
+    public List<en_MessageType> _ChattingBoxProperty = new List<en_MessageType>();
+
+    public override void Init()
+    {
+        ChattingMessageList = new List<CChattingMessage>();
+                
+        Bind<GameObject>(typeof(en_ChattingBoxGameObject));
+        Bind<Button>(typeof(en_ChattingBoxButton));
+        Bind<TextMeshProUGUI>(typeof(en_ChattingBoxText));
+
+        BindEvent(GetButton((int)en_ChattingBoxButton.ChattingSubjectButton).gameObject, OnChattingBoxSubjectButtonClick, Define.en_UIEvent.MouseClick);
+        
+    }
+
+    public void SetChattingMessage(string SendUser, string ChattingText, en_MessageType MessageType, st_Color MessageColor)
+    {
+        // 채팅메세지 개수가 최대 갯수 이상이면
+        if (ChattingMessageList.Count >= MaxMessage)
+        {
+            // 맨 앞에 있는 채팅 메세지를 삭제한다.
+            Destroy(ChattingMessageList[0].ChattingTextObject.gameObject);
+            ChattingMessageList.Remove(ChattingMessageList[0]);
+        }
+
+        // 채팅메세지를 새로 생성한다.
+        CChattingMessage ChattingMessage = new CChattingMessage();
+        ChattingMessage.ChattingTextObject = Managers.Resource.Instantiate(en_ResourceName.CLIENT_UI_CHATTING_TEXT, Get<GameObject>((int)en_ChattingBoxGameObject.ChattingPannel).transform).GetComponent<TextMeshProUGUI>();
+
+        Color32 Color = new Color32();
+        Color.r = (byte)MessageColor._Red;
+        Color.g = (byte)MessageColor._Green;
+        Color.b = (byte)MessageColor._Blue;
+        Color.a = 255;
+
+        //-----------------------------------------------
+        // 선택되지 않은 채팅창은 숨겨줘야하는 작업 필요 
+        //-----------------------------------------------
+
+        // 메세지 타입에 따라 색을 다르게 지정해서 채팅메세지를 셋팅한다.
+        switch (MessageType)
+        {
+            case en_MessageType.MESSAGE_TYPE_CHATTING:
+                string CommonText = SendUser + " : " + ChattingText;
+                ChattingMessage.ChattingTextObject.color = Color;
+                ChattingMessage.ChattingTextObject.text = CommonText;
+                break;
+            case en_MessageType.MESSAGE_TYPE_SYSTEM:
+                string SystemText = ChattingText;
+                ChattingMessage.ChattingTextObject.color = Color;
+                ChattingMessage.ChattingTextObject.text = SystemText;
+                break;
+        }
+
+        // 배열에 추가한다.
+        ChattingMessageList.Add(ChattingMessage);
+    }
+
+    public void SetDamageChattingMessage(string AttackerName, string TargetName, en_SkillType SkillType, int Damage)
+    {
+        // 채팅메세지 개수가 최대 갯수 이상이면
+        if (ChattingMessageList.Count >= MaxMessage)
+        {
+            // 맨 앞에 있는 채팅 메세지를 삭제한다.
+            Destroy(ChattingMessageList[0].ChattingTextObject.gameObject);
+            ChattingMessageList.Remove(ChattingMessageList[0]);
+        }
+
+        // 채팅메세지를 새로 생성한다.
+        CChattingMessage DamageChattingMessage = new CChattingMessage();
+        DamageChattingMessage.ChattingTextObject = Managers.Resource.Instantiate(en_ResourceName.CLIENT_UI_CHATTING_TEXT, Get<GameObject>((int)en_ChattingBoxGameObject.ChattingPannel).transform).GetComponent<TextMeshProUGUI>();      
+
+        string CommonText = AttackerName + "이 " + Managers.String._SkillString[SkillType] + "을 사용해" + TargetName + "에게 " + Damage.ToString() + "의 데미지를 줬습니다.";
+        DamageChattingMessage.ChattingTextObject.text = CommonText;        
+
+        // 배열에 추가한다.
+        ChattingMessageList.Add(DamageChattingMessage);
+    }
+
+    void OnChattingBoxSubjectButtonClick(PointerEventData Event)
+    {
+        UI_GameScene GameSceneUI = Managers.UI._SceneUI as UI_GameScene;
+        if(GameSceneUI == null)
+        {
+            return;
+        }
+
+        GameSceneUI._ChattingBoxGroup.SelectChattingBox(_ChattingBoxIndex);        
+    }
+
+    public void ChattingScrollBoxActive(bool Active)
+    {
+        GetGameObject((int)en_ChattingBoxGameObject.ChattingBox).gameObject.SetActive(Active);
+    }
+
+    public GameObject GetChattingViewSubject()
+    {
+        return GetGameObject((int)en_ChattingBoxGameObject.ChattingViewSubject).gameObject;
+    }
+
+    public void SetChattingBoxName(string BoxName)
+    {
+        GetTextMeshPro((int)en_ChattingBoxText.ChattingSubjectText).text = BoxName;
+    }
+}
---
+++ Assets/Scripts/Client/UI/ChattingBox/UI_ChattingBoxGroup.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using static UI_ChattingBox;
+
+public class UI_ChattingBoxGroup : UI_Scene
+{
+    enum en_ChattingBoxGroupGameObject
+    {
+        ChattingBoxNames,
+        ChattingBoxes
+    }
+
+    enum en_ChattingBoxGroupInputField
+    {
+        ChattingInputField // 채팅메세지 입력창
+    }
+
+    enum en_ChattingBoxGroupText
+    {
+        ChattingRemainLengthText
+    }
+
+    List<UI_ChattingBox> _ChattingBoxUIs = new List<UI_ChattingBox>();
+    
+    public override void Init()
+    {
+        Bind<InputField>(typeof(en_ChattingBoxGroupInputField));
+        Bind<GameObject>(typeof(en_ChattingBoxGroupGameObject));
+        Bind<TextMeshProUGUI>(typeof(en_ChattingBoxGroupText));
+
+        // 채팅 박스 2개 생성
+        for (int i = 0; i < 2; i++)
+        {
+            GameObject ChattingBoxUIGo = Managers.Resource.Instantiate(en_ResourceName.CLIENT_UI_CHATTING_BOX, GetGameObject((int)en_ChattingBoxGroupGameObject.ChattingBoxes).transform);
+            if (ChattingBoxUIGo != null)
+            {
+                UI_ChattingBox ChattingBoxUI = ChattingBoxUIGo.GetComponent<UI_ChattingBox>();
+                if (ChattingBoxUI != null)
+                {
+                    ChattingBoxUI._ChattingBoxProperty.Add((en_MessageType)i);
+                    ChattingBoxUI._ChattingBoxIndex = i;
+                    ChattingBoxUI.GetChattingViewSubject().transform.SetParent(GetGameObject((int)en_ChattingBoxGroupGameObject.ChattingBoxNames).transform);
+                    _ChattingBoxUIs.Add(ChattingBoxUI);
+                    
+                    if (i == 1)
+                    {
+                        ChattingBoxUI.ChattingScrollBoxActive(false);
+                        ChattingBoxUI.SetChattingBoxName("전투");
+                    }
+                }
+            }
+        }
+
+        Get<InputField>((int)en_ChattingBoxGroupInputField.ChattingInputField).gameObject.SetActive(false);
+    }
+
+
+    public override void Binding()
+    {
+    }
+                                                                                        
+    public override void ShowCloseUI(bool IsShowClose)
+    {
+        gameObject.SetActive(IsShowClose);
+    }
+
+    // 채팅 메세지 생성
+    public void NewChattingMessage(string SendUser, string ChattingText, en_MessageType MessageType, st_Color MessageColor)
+    {
+        foreach (UI_ChattingBox ChattingBoxUI in _ChattingBoxUIs)
+        {
+            foreach (en_MessageType ChattingBoxProperty in ChattingBoxUI._ChattingBoxProperty)
+            {
+                if (ChattingBoxProperty == MessageType)
+                {
+                    ChattingBoxUI.SetChattingMessage(SendUser, ChattingText, MessageType, MessageColor);
+                }
+            }
+        }
+    }
+
+    public void NewDamageChattingMessage(en_MessageType MessageType, string AttackerName, string TargetName, en_SkillType SkillType, int Damage)
+    {
+        foreach(UI_ChattingBox ChattingBoxUI in _ChattingBoxUIs)
+        {
+            foreach (en_MessageType ChattingBoxProperty in ChattingBoxUI._ChattingBoxProperty)
+            {
+                if (ChattingBoxProperty == MessageType)
+                {
+                    ChattingBoxUI.SetDamageChattingMessage(AttackerName, TargetName, SkillType, Damage);
+                }
+            }
+        }
+    }
+
+    public InputField GetChattingInputField()
+    {
+        return Get<InputField>((int)en_ChattingBoxGroupInputField.ChattingInputField);
+    }
+
+    public void SelectChattingBox(int Index)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            if (i == Index)
+            {
+                _ChattingBoxUIs[i].ChattingScrollBoxActive(true);
+            }
+            else
+            {
+                _ChattingBoxUIs[i].ChattingScrollBoxActive(false);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if(GetChattingInputField() != null && GetChattingInputField().IsActive() == true)
+        {
+            GetTextMeshPro((int)en_ChattingBoxGroupText.ChattingRemainLengthText).text = (50 - GetChattingInputField().text.Length).ToString();            
+        }
+    }   
+}
