d168cbc6a9941677765c37a7a9fde047878478c3
정성원 <tyhg56@naver.com>
2023-02-08 08:54:45 +0900

    Feat : " 서버 선택 UI 추가 "
    UI_SelectServer, UI_SelectServerItem ( 서버 선택 UI )

---
+++ Assets/Scripts/Client/UI/SelectServer/UI_SelectServer.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UI_SelectServer : UI_Scene
+{ 
+    // 서버 목록 UI 버튼들
+    public List<UI_SelectServerItem> _SelectServerItems { get; } = new List<UI_SelectServerItem>();
+
+    enum en_SelectServerPopupGameObject
+    {
+        SelectServerPanel,
+        ServerSelectClose
+    }
+
+    public override void Init()
+    {
+        Bind<GameObject>(typeof(en_SelectServerPopupGameObject));
+
+        BindEvent(GetGameObject((int)en_SelectServerPopupGameObject.SelectServerPanel).gameObject, OnSelectServerDrag, Define.en_UIEvent.Drag);
+        BindEvent(GetGameObject((int)en_SelectServerPopupGameObject.ServerSelectClose).gameObject, OnSelectServerClose, Define.en_UIEvent.MouseClick);
+    }
+
+    private void OnSelectServerDrag(PointerEventData Event)
+    {
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        rectTransform.anchoredPosition += Event.delta;
+    }
+
+    private void OnSelectServerClose(PointerEventData Event)
+    {
+        UISelectServerVisivle(false);
+    }
+
+    public void UISelectServerVisivle(bool Active)
+    {
+        gameObject.SetActive(Active);
+    }
+
+    // 받은 서버정보로 버튼 생성 및 초기화 해준다.
+    public void SetSelectServer(st_ServerInfo[] ServerInfos, byte Count)
+    {
+        UISelectServerVisivle(true);        
+
+        // 기존에 가지고 있던 서버 목록 다 없애주고
+        _SelectServerItems.Clear(); 
+
+        // 프리팹에 있는 서버목록 버튼들도 다 없애준다.
+        GameObject ServerGrid = GetComponentInChildren<GridLayoutGroup>().gameObject;
+        foreach (Transform Child in ServerGrid.transform)
+        {
+            Destroy(Child.gameObject);
+        }
+        
+        // 서버목록 개수 만큼 버튼 만들어서 Grid에 자식으로 붙이고 저장해준 후
+        // 서버정보를 셋팅해준다.
+        for (int i = 0; i < Count; i++)
+        {
+            GameObject ServerSelectPopItemGo = Managers.Resource.Instantiate(en_ResourceName.CLIENT_UI_SERVER_SELECT_ITEM, ServerGrid.transform);
+            UI_SelectServerItem SelectServerItem = Util.GetOrAddComponent<UI_SelectServerItem>(ServerSelectPopItemGo);
+            _SelectServerItems.Add(SelectServerItem);
+            
+            SelectServerItem._ServerInfo = ServerInfos[i];
+            // 추가적으로 BusyScore에 따라 초록색에서 빨간색으로 표시
+        }
+
+        // RefreshUI를 이용해서 UI를 업데이트 해준다.
+        RefreshUI();
+    }
+
+    public void RefreshUI()
+    {
+        if (_SelectServerItems.Count == 0)
+        {
+            return;
+        }
+
+        // 가지고 있는 서버 목록 버튼 만큼 반복하면서 UI를 업데이트 해준다.
+        foreach (UI_SelectServerItem SelectServerItem in _SelectServerItems)
+        {
+            SelectServerItem.SelectServerPopupItemRefresh();
+        }
+    }    
+}
---
+++ Assets/Scripts/Client/UI/SelectServer/UI_SelectServerItem.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UI_SelectServerItem : UI_Scene
+{
+    // 서버목록 버튼이 들고 있는 서버정보
+    public st_ServerInfo _ServerInfo { get; set; }
+
+    enum en_ServerSelectButton
+    {
+        SelectServerButton
+    }
+
+    enum en_ServerSelectText
+    {
+        NameText
+    }
+
+    enum en_Buttons
+    {
+        CreateButton,
+        LoginButton
+    }
+
+    public override void Init()
+    {
+        // SelectServerButton를 가져온다.
+        Bind<Button>(typeof(en_ServerSelectButton));
+        // NameText를 가져온다.
+        Bind<Text>(typeof(en_ServerSelectText));
+
+        // SelectServerButton에 클릭이벤트를 달고 클릭했을때 OnServerSelectButtonClick를 호출 하도록 해준다.
+        BindEvent(GetButton((int)en_ServerSelectButton.SelectServerButton).gameObject, OnSeverSelectButtonClick,Define.en_UIEvent.MouseClick);
+    }
+
+    // 버튼 UI 업데이트
+    public void SelectServerPopupItemRefresh()
+    {
+        if(_ServerInfo.ServerName == null)
+        {
+            return;
+        }
+
+        // NameText를 서버이름으로 바꿔준다.
+        GetText((int)en_ServerSelectText.NameText).text = _ServerInfo.ServerName;
+    }
+
+    // 서버 목록 버튼을 클릭할 경우 게임서버 접속
+    void OnSeverSelectButtonClick(PointerEventData Event)
+    {
+        // 게임서버에 접속
+        Managers.NetworkManager.ConnectToGame(_ServerInfo);
+
+        UI_LoginScene LoginScene = Managers.UI._SceneUI as UI_LoginScene;
+        if(LoginScene != null)
+        {
+            LoginScene._SelectServerUI.UISelectServerVisivle(false);
+        }
+    }
+}
