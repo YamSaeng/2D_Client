9c70202675f71c6d4d0f35ba9219efd242449e6b
정성원 <tyhg56@naver.com>
2023-02-07 14:34:45 +0900

    Feat : " SkillBoxManager 추가 "
    SkillBoxManager 추가 ( 캐릭터 스킬 관리 )

---
+++ Assets/Scripts/Client/Managers/Contents/SkillBoxManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillBoxManager
+{    
+    public SkillCharacteristic _PublicCharacteristic = new SkillCharacteristic();
+    public SkillCharacteristic[] _Characteristics = new SkillCharacteristic[3];
+
+    public SkillBoxManager()
+    {
+
+    }
+
+    public void Init()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            _Characteristics[i] = new SkillCharacteristic();
+        }
+    }    
+
+    public void CreatePublicChracteristicPassive(en_SkillCharacteristic PublicCharacteristicType,
+        byte PublicPassiveSkillCount, st_SkillInfo[] PublicPassiveSkillInfos)
+    {
+        _PublicCharacteristic.PassiveSkillCharacteristicInit(PublicCharacteristicType, PublicPassiveSkillCount, PublicPassiveSkillInfos);
+    }
+
+    public void CreatePublicChracteristicActive(en_SkillCharacteristic PublicCharacteristicType,
+        byte PublicActiveSkillCount, st_SkillInfo[] PassiveSkillInfos)
+    {
+        _PublicCharacteristic.ActiveSkillCharacteristicInit(PublicCharacteristicType, PublicActiveSkillCount, PassiveSkillInfos);
+    }
+
+    public void CreateChracteristicPassive(byte ChracteristicIndex, en_SkillCharacteristic CharacteristicType,
+        byte PassiveSkillCount, st_SkillInfo[] PassiveSkillInfos)
+    {
+        _Characteristics[ChracteristicIndex].PassiveSkillCharacteristicInit(CharacteristicType, PassiveSkillCount, PassiveSkillInfos);
+    }
+
+    public void CreateChracteristicActive(byte ChracteristicIndex, en_SkillCharacteristic ChracteristicType,
+        byte ActiveSkillCount, st_SkillInfo[] ActiveSkillInfos)
+    {
+        _Characteristics[ChracteristicIndex].ActiveSkillCharacteristicInit(ChracteristicType, ActiveSkillCount, ActiveSkillInfos);
+    }
+
+    public void SetSkillLearn(short SkillType, bool IsSkillLearn)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            st_SkillInfo Skill = _Characteristics[i].FindSkill(SkillType);
+            if(Skill != null)
+            {
+                Skill.IsSkillLearn = IsSkillLearn;
+                break;
+            }
+        }
+    }
+}
