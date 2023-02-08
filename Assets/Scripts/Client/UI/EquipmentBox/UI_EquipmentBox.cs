d37f57033a67a2659869be294eed701e184d542b
정성원 <tyhg56@naver.com>
2023-02-08 09:56:01 +0900

    Feat : " 장비 창 UI 추가 "
    UI_EquipmentBox 추가 ( 장비 창 UI )
    
    UI_EquipmentItem 추가 ( 장비 창 아이템 UI )

---
+++ Assets/Scripts/Client/UI/EquipmentBox/UI_EquipmentBox.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UI_EquipmentBox : UI_Base
+{
+    enum en_EquipmentBoxGameObjects
+    {
+        UI_EquipmentBox,
+        UI_HeadEquipmentItem,
+        UI_BodyEquipmentItem,
+        UI_BootEquipmentItem,
+        UI_LeftHandEquipmentItem,
+        UI_RightHandEquipmentItem
+    }
+
+    enum en_EquipmentBoxTexts
+    {
+        UserNameText,
+        UserLevelText,
+        UserJobText,
+        HealthPointText,
+        ManaPointText,
+        MeleeAttackPointText,
+        MeleeAttackHitRatePointText,
+        MeleeCriticalPointText,
+        MagicDamagePointText,
+        MagicHitRatePointText,
+        MagicCriticalPointText,
+        DefencePointText,
+        EvasionRatePointText
+    }
+
+    public int _WeaponMinDamage = 0;
+    public int _WeaponMaxDamage = 0;
+    public int _HeadArmorDefence = 0;
+    public int _WearArmorDefence = 0;
+    public int _BootArmorDefence = 0;
+
+    UI_EquipmentItem[] _EquipmentParts;    
+
+    public override void Init()
+    {
+
+    }    
+
+    public override void ShowCloseUI(bool IsShowClose)
+    {
+        gameObject.SetActive(IsShowClose);
+    }
+
+    public override void Binding()
+    {
+        Bind<GameObject>(typeof(en_EquipmentBoxGameObjects));
+        Bind<TextMeshProUGUI>(typeof(en_EquipmentBoxTexts));
+
+        BindEvent(GetGameObject((int)en_EquipmentBoxGameObjects.UI_EquipmentBox).gameObject, OnEquipmentBoxDrag, Define.en_UIEvent.Drag);
+
+        GetGameObject((int)en_EquipmentBoxGameObjects.UI_HeadEquipmentItem).GetComponent<UI_EquipmentItem>().Binding();
+        GetGameObject((int)en_EquipmentBoxGameObjects.UI_BodyEquipmentItem).GetComponent<UI_EquipmentItem>().Binding();        
+        GetGameObject((int)en_EquipmentBoxGameObjects.UI_LeftHandEquipmentItem).GetComponent<UI_EquipmentItem>().Binding();
+        GetGameObject((int)en_EquipmentBoxGameObjects.UI_RightHandEquipmentItem).GetComponent<UI_EquipmentItem>().Binding();
+        GetGameObject((int)en_EquipmentBoxGameObjects.UI_BootEquipmentItem).GetComponent<UI_EquipmentItem>().Binding();
+    }
+
+    public void EquipmentBoxUICreate(byte EquipmentPartCount)
+    {
+        _EquipmentParts = new UI_EquipmentItem[EquipmentPartCount];
+
+        _EquipmentParts[(int)en_EquipmentParts.EQUIPMENT_PARTS_HEAD] = GetGameObject((int)en_EquipmentBoxGameObjects.UI_HeadEquipmentItem).GetComponent<UI_EquipmentItem>();
+        _EquipmentParts[(int)en_EquipmentParts.EQUIPMENT_PARTS_BODY] = GetGameObject((int)en_EquipmentBoxGameObjects.UI_BodyEquipmentItem).GetComponent<UI_EquipmentItem>();
+        _EquipmentParts[(int)en_EquipmentParts.EQUIPMENT_PARTS_LEFT_HAND] = GetGameObject((int)en_EquipmentBoxGameObjects.UI_LeftHandEquipmentItem).GetComponent<UI_EquipmentItem>();
+        _EquipmentParts[(int)en_EquipmentParts.EQUIPMENT_PARTS_RIGHT_HAND] = GetGameObject((int)en_EquipmentBoxGameObjects.UI_RightHandEquipmentItem).GetComponent<UI_EquipmentItem>();
+        _EquipmentParts[(int)en_EquipmentParts.EQUIPMENT_PARTS_BOOT] = GetGameObject((int)en_EquipmentBoxGameObjects.UI_BootEquipmentItem).GetComponent<UI_EquipmentItem>();
+    }
+
+    private void OnEquipmentBoxDrag(PointerEventData Event)
+    {
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        rectTransform.anchoredPosition += Event.delta;
+    }
+
+    public void OnEquipmentItem(st_ItemInfo OnEquipmentItemInfo)
+    {
+        _EquipmentParts[(int)OnEquipmentItemInfo.ItemEquipmentPart].SetEquipment(OnEquipmentItemInfo);
+
+        EquipmentBoxRefreshUI();
+    }
+
+    public void OffEquipmentItem(en_EquipmentParts OffEquipmentParts)
+    {
+        _EquipmentParts[(int)OffEquipmentParts].InitEquipmentItemUI();
+
+        EquipmentBoxRefreshUI();
+    }
+   
+    public void EquipmentBoxRefreshUI()
+    {
+        GameObject FindPlayerGO = Managers.Object.FindById(Managers.NetworkManager._PlayerDBId);
+        if (FindPlayerGO != null)
+        {
+            PlayerObject Player = FindPlayerGO.GetComponent<PlayerObject>();
+
+            GetTextMeshPro((int)en_EquipmentBoxTexts.UserNameText).text = Player._GameObjectInfo.ObjectName;
+            GetTextMeshPro((int)en_EquipmentBoxTexts.HealthPointText).text = $"레벨 {Player._GameObjectInfo.ObjectStatInfo.Level}";            
+
+            GetTextMeshPro((int)en_EquipmentBoxTexts.HealthPointText).text = $"체력 {Player._GameObjectInfo.ObjectStatInfo.HP} / {Player._GameObjectInfo.ObjectStatInfo.MaxHP}";
+            GetTextMeshPro((int)en_EquipmentBoxTexts.ManaPointText).text = $"마력 {Player._GameObjectInfo.ObjectStatInfo.MP} / {Player._GameObjectInfo.ObjectStatInfo.MaxMP}";
+
+            GetTextMeshPro((int)en_EquipmentBoxTexts.MeleeAttackPointText).text = $"근접 공격력 {_WeaponMinDamage + Player._GameObjectInfo.ObjectStatInfo.MinMeleeAttackDamage} ~ {_WeaponMaxDamage + Player._GameObjectInfo.ObjectStatInfo.MaxMeleeAttackDamage}";
+            GetTextMeshPro((int)en_EquipmentBoxTexts.MeleeAttackHitRatePointText).text = $"근접 명중률 {Player._GameObjectInfo.ObjectStatInfo.MeleeAttackHitRate}";
+            GetTextMeshPro((int)en_EquipmentBoxTexts.MeleeCriticalPointText).text = $"근접 치명타 {Player._GameObjectInfo.ObjectStatInfo.MeleeCriticalPoint}";
+            GetTextMeshPro((int)en_EquipmentBoxTexts.MagicDamagePointText).text = $"마법 공격력 {Player._GameObjectInfo.ObjectStatInfo.MagicDamage}";
+            GetTextMeshPro((int)en_EquipmentBoxTexts.MagicHitRatePointText).text = $"마법 명중률 {Player._GameObjectInfo.ObjectStatInfo.MagicHitRate}";
+            GetTextMeshPro((int)en_EquipmentBoxTexts.MagicCriticalPointText).text = $"마법 치명타 {Player._GameObjectInfo.ObjectStatInfo.MagicCriticalPoint}";
+            GetTextMeshPro((int)en_EquipmentBoxTexts.DefencePointText).text = $"방어력 {_HeadArmorDefence + _WearArmorDefence + _BootArmorDefence + Player._GameObjectInfo.ObjectStatInfo.Defence}";
+            GetTextMeshPro((int)en_EquipmentBoxTexts.EvasionRatePointText).text = $"회피율 {Player._GameObjectInfo.ObjectStatInfo.EvasionRate}";
+        }
+    }  
+}
---
+++ Assets/Scripts/Client/UI/EquipmentBox/UI_EquipmentItem.cs
@@ -0,0 +1,86 @@
+﻿using ServerCore;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UI_EquipmentItem : UI_Base
+{
+    private st_ItemInfo _EquipmentItemInfo;
+
+    enum en_EquipmentItemImage
+    {
+        EquipmentItemImage
+    }
+
+    public Sprite _InitImage;
+
+    UI_GameScene _GameSceneUI;
+
+    public override void Init()
+    {
+
+    }
+
+    public override void Binding()
+    {
+        Bind<Image>(typeof(en_EquipmentItemImage));
+
+        BindEvent(GetImage((int)en_EquipmentItemImage.EquipmentItemImage).gameObject, OnEquipmentItemClick, Define.en_UIEvent.MouseClick);
+        BindEvent(GetImage((int)en_EquipmentItemImage.EquipmentItemImage).gameObject, OnEquipmentItemPointerEnter, Define.en_UIEvent.PointerEnter);
+        BindEvent(GetImage((int)en_EquipmentItemImage.EquipmentItemImage).gameObject, OnEquipmentItemPointerExit, Define.en_UIEvent.PointerExit);
+
+    }
+
+    public override void ShowCloseUI(bool IsShowClose)
+    {
+        gameObject.SetActive(IsShowClose);
+    }
+
+    private void OnEquipmentItemClick(PointerEventData EquipmentItemClickEvent)
+    {
+        if(EquipmentItemClickEvent.button == PointerEventData.InputButton.Right)
+        {
+            if(EquipmentItemClickEvent.clickCount == 1)
+            {
+                CMessage ReqOffEquipmentPacket = Packet.MakePacket.ReqMakeOffEquipmentPacket(Managers.NetworkManager._AccountId,
+                    Managers.NetworkManager._PlayerDBId,
+                    _EquipmentItemInfo.ItemEquipmentPart);
+                Managers.NetworkManager.GameServerSend(ReqOffEquipmentPacket);                
+            }
+        }
+    }
+
+    private void OnEquipmentItemPointerEnter(PointerEventData EquipmentItemClickEvent)
+    {
+        //if (Managers.Inventory.SelectedItem == null && _EquipmentItemInfo != null)
+        //{
+        //    _GameSceneUI = Managers.UI._SceneUI as UI_GameScene;
+
+        //    _GameSceneUI.SetItemExplanation(_EquipmentItemInfo);
+        //}
+    }
+
+    private void OnEquipmentItemPointerExit(PointerEventData EquipmentItemClickEvent)
+    {
+        _GameSceneUI = Managers.UI._SceneUI as UI_GameScene;
+
+        _GameSceneUI.EmptyItemExplanation();
+    }
+
+    public void SetEquipment(st_ItemInfo EquipmentItemInfo)
+    {
+        _EquipmentItemInfo = EquipmentItemInfo;
+
+        GetImage((int)en_EquipmentItemImage.EquipmentItemImage).sprite = Managers.Sprite._ItemSprite[EquipmentItemInfo.ItemSmallCategory];
+    }
+
+    public void InitEquipmentItemUI()
+    {
+        _EquipmentItemInfo = null;
+
+        GetImage((int)en_EquipmentItemImage.EquipmentItemImage).sprite = _InitImage;
+    }
+}
+
