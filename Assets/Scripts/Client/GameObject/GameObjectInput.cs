ecfb2a9e6c2ca92774752c943cf03ddae5a0ea79
정성원 <tyhg56@naver.com>
2023-02-08 10:39:59 +0900

    Feat : " GameObjectInput 추가 "
    GameObjectInput 추가 ( GameObject에 들어오는 Input 값들을 관리 )

---
+++ Assets/Scripts/Client/GameObject/GameObjectInput.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameObjectInput : MonoBehaviour
+{
+    private Camera _MainCamera;    
+    private bool _IsDefaultAttack = false;
+
+    [field: SerializeField]
+    public UnityEvent<Vector2> OnMovementKeyPressed { get; set; }
+
+    [field: SerializeField]
+    public UnityEvent<Vector2> OnPointerPositionChange { get; set; }
+
+    [field: SerializeField]
+    public UnityEvent OnDefaultAttackPressed { get; set; }
+
+    [field: SerializeField]
+    public UnityEvent OnDefaultAttackReleased { get; set; }
+
+    private void Awake()
+    {
+        _MainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        GetUIKeyInput();
+        GetPointerInput();
+        GetMovementInput();
+        GetDefaultAttackInput();
+    }
+
+    private void GetUIKeyInput()
+    {
+        
+    }
+
+    private void GetDefaultAttackInput()
+    {
+        if (Input.GetAxisRaw("DefaultAttack") > 0)
+        {            
+            if (_IsDefaultAttack == false)
+            {
+                _IsDefaultAttack = true;
+                OnDefaultAttackPressed?.Invoke();
+            }
+        }
+        else
+        {
+            if (_IsDefaultAttack == true)
+            {
+                _IsDefaultAttack = false;
+                OnDefaultAttackReleased?.Invoke();
+            }
+        }
+    }
+
+    private void GetPointerInput()
+    {
+        if (Input.GetMouseButton(1) == true)
+        {
+            Vector3 ScreenMousePosition = _MainCamera.ScreenToWorldPoint(Input.mousePosition);
+            OnPointerPositionChange?.Invoke(ScreenMousePosition);
+        }        
+    }
+
+    private void GetMovementInput()
+    {
+        Vector2 Direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        OnMovementKeyPressed?.Invoke(Direction);
+    }
+}
