f50d80528404bcb2897787483c14db5c7ada30a4
정성원 <tyhg56@naver.com>
2023-03-19 20:17:08 +0900

    Fix : " GoblinObject 수정 "
    GoblinObject
     - GolinObject -> CGoblinObject 이름 수정
     - Init() 수정
      - HPBar, NameUI 추가
     - UpdateController()
      - MOVING, ATTACK 상태일때 바라보는 방향과 무기가 바라보는 방향 재설정

--- Assets/Scripts/Client/GameObject/Monster/GoblinObject.cs
+++ Assets/Scripts/Client/GameObject/Monster/GoblinObject.cs
@@ -2,15 +2,89 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoblinObject : CreatureObject
+public class CGoblinObject : CreatureObject
 {
     public override void Init()
     {
         base.Init();
+
+        if (_HpBar == null)
+        {
+            AddHPBar(0.5f, 0.35f);
+        }
+
+        if(_NameUI == null)
+        {
+            AddNameBar(0.5f, -0.2f);
+        }
+
+        _NameUI.gameObject.SetActive(true);
     }
 
     protected override void UpdateController()
     {
+        switch(_GameObjectInfo.ObjectPositionInfo.State)
+        {
+            case en_CreatureState.IDLE:
+                break;
+            case en_CreatureState.MOVING:
+                if(_TargetObject != null)
+                {
+                    Transform RendererTransform = transform.Find("GameObjectRenderer");
+                    if(RendererTransform != null)
+                    {
+                        GameObjectRenderer Renderer = RendererTransform.GetComponent<GameObjectRenderer>();
+                        if(Renderer != null)
+                        {
+                            Renderer.FaceDirection(new Vector2(_TargetObject.gameObject.transform.position.x,
+                                _TargetObject.gameObject.transform.position.y));
+                        }
+                    }
 
+                    Transform RightWeaponParentTransform = transform.Find("RightWeaponParent");
+                    if (RightWeaponParentTransform != null)
+                    {
+                        GameObject RightWeaponParent = RightWeaponParentTransform.gameObject;
+                        if (RightWeaponParent != null)
+                        {
+                            PlayerWeapon Weapon = RightWeaponParent.GetComponent<PlayerWeapon>();
+                            if (Weapon != null)
+                            {
+                                Weapon.AimWeapon(new Vector2(_TargetObject.gameObject.transform.position.x, _TargetObject.gameObject.transform.position.y));
+                            }
+                        }
+                    }
+                }
+                break;
+            case en_CreatureState.ATTACK:
+                if(_TargetObject != null)
+                {
+                    Transform RightWeaponParentTransform = transform.Find("RightWeaponParent");
+                    if (RightWeaponParentTransform != null)
+                    {
+                        GameObject RightWeaponParent = RightWeaponParentTransform.gameObject;
+                        if (RightWeaponParent != null)
+                        {
+                            PlayerWeapon Weapon = RightWeaponParent.GetComponent<PlayerWeapon>();
+                            if (Weapon != null)
+                            {
+                                Weapon.AimWeapon(new Vector2(_TargetObject.gameObject.transform.position.x, _TargetObject.gameObject.transform.position.y));
+                            }
+                        }
+                    }
+
+                    Transform RendererTransform = transform.Find("GameObjectRenderer");
+                    if (RendererTransform != null)
+                    {
+                        GameObjectRenderer Renderer = RendererTransform.GetComponent<GameObjectRenderer>();
+                        if (Renderer != null)
+                        {
+                            Renderer.FaceDirection(new Vector2(_TargetObject.gameObject.transform.position.x,
+                                _TargetObject.gameObject.transform.position.y));
+                        }
+                    }
+                }                
+                break;
+        }
     }    
 }
