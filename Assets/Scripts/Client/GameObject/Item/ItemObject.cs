97b7a26b3dbac9ea5ffe0f99f183ce41d12638de
정성원 <tyhg56@naver.com>
2023-03-19 20:15:05 +0900

    Fix : " ItemController 수정 "
    ItemController
     - ItemController -> CItemObject 이름 수정
     - 대상에게 일정 거리 안에 접근하면 오브젝트 풀에 반환하도록 수정

---
+++ Assets/Scripts/Client/GameObject/Item/ItemObject.cs
@@ -0,0 +1,73 @@
+﻿using ServerCore;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CItemObject : CreatureObject
+{
+    public PlayerObject _Target;
+    public en_ItemState _ItemState;    
+    public float _MovePower = 10.0f;    
+
+    public override void Init()
+    {
+        base.Init();
+
+        _ItemState = en_ItemState.ITEM_IDLE;
+    }
+
+    IEnumerator CoTargetFly(float Time)
+    {
+        yield return new WaitForSeconds(Time);
+
+        _ItemState = en_ItemState.ITEM_READY_MOVE;
+    }    
+
+    public void TargetFlyStart(float TargetSpeed)
+    {
+        _MovePower = TargetSpeed * 2.0f;
+
+        StartCoroutine("CoTargetFly", 2.0f);    
+    }
+
+    void Update()
+    {
+        switch (_ItemState)
+        {
+            case en_ItemState.ITEM_IDLE:
+                break;
+            case en_ItemState.ITEM_READY_MOVE:
+                _ItemState = en_ItemState.ITEM_MOVE;
+                break;
+            case en_ItemState.ITEM_MOVE:
+                if (_Target != null)
+                {
+                    GameObjectRenderer TargetRenderer = _Target.transform.Find("GameObjectRenderer").GetComponent<GameObjectRenderer>();
+                    if(TargetRenderer != null)
+                    {
+                        GameObject Target = TargetRenderer.gameObject;
+
+                        // 타겟까지의 방향값을 구함
+                        Vector3 TargetDir = (Target.transform.position - transform.transform.position).normalized;
+
+                        // 타겟까지의 거리를 구함
+                        float Distance = Vector3.Distance(Target.transform.position, transform.position);
+
+                        // 거리가 0.5보다 크다면
+                        if (Distance > 0.5f)
+                        {
+                            // 타겟한테 다가감
+                            //transform.position = transform.position + TargetDir * Distance * MOVE_POWER * Time.deltaTime;                
+                            Vector3 newPosition = Vector3.MoveTowards(transform.position, Target.transform.position, _MovePower * Time.deltaTime);
+                            transform.position = newPosition;
+                        }
+                        else
+                        {
+                            Managers.Object.Remove(_GameObjectInfo.ObjectId);
+                        }
+                    }                    
+                }
+                break;
+        }        
+    }  
+}
