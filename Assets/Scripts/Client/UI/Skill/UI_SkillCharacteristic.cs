7459883564acd666a1fa717dbd05e8d0dac903d1
정성원 <tyhg56@naver.com>
2023-03-24 07:42:20 +0900

    Fix : " UI_SkillCharacteristic 수정 "
    UI_SkillCharacteristic
     - _SkillCharacteristicIndex 삭제

--- Assets/Scripts/Client/UI/Skill/UI_SkillCharacteristic.cs
+++ Assets/Scripts/Client/UI/Skill/UI_SkillCharacteristic.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class UI_SkillCharacteristic : UI_Base
-{
-    public byte _SkillCharacteristicIndex;
+{    
     private List<UI_SkillItem> _PassiveSkillItemUI = new List<UI_SkillItem>();
     private List<UI_SkillItem> _ActiveSkillItemUI = new List<UI_SkillItem>();
 
@@ -15,7 +15,7 @@ public class UI_SkillCharacteristic : UI_Base
     enum en_SkillChracteristicButton
     {
         SelectCharcteristicButton
-    }
+    }  
 
     public override void Init()
     {
@@ -24,7 +24,7 @@ public class UI_SkillCharacteristic : UI_Base
 
     public override void Binding()
     {
-        Bind<Button>(typeof(en_SkillChracteristicButton));
+        Bind<Button>(typeof(en_SkillChracteristicButton));        
 
         BindEvent(GetButton((int)en_SkillChracteristicButton.SelectCharcteristicButton).gameObject, OnSelectChracteristicButtonClick, Define.en_UIEvent.MouseClick);
     }    
@@ -39,7 +39,7 @@ public class UI_SkillCharacteristic : UI_Base
         if(_SkillBox != null)
         {
             _SkillBox.SkillBoxCharacteristicShowClose(false);
-            _SkillBox.SkillBoxSelectCharacteristicShowClose(true, _SkillCharacteristicIndex);
+            _SkillBox.SkillBoxSelectCharacteristicShowClose(true);
             _SkillBox.SkillBoxButtonShowClose(false);
         }
     }
