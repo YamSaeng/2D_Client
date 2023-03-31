7b0292e2fc18326fe5efaf55eb79b08800aeb789
정성원 <tyhg56@naver.com>
2023-03-31 16:52:16 +0900

    Fix : " UI_SkillExplanation 수정 "
    UI_SkillExplanation
     - en_SkillExplanationText 수정
      - SkillDamage, SkillMin, SkillMaxDamage Type 추가
     - SkillExplanationSet() 수정
      - SkillType에 따라 설명을 따로 설정

--- Assets/Scripts/Client/UI/Skill/UI_SkillExplanation.cs
+++ Assets/Scripts/Client/UI/Skill/UI_SkillExplanation.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class UI_SkillExplanation : UI_Base
 {
-    st_SkillInfo _SkillInfo;    
+    st_SkillInfo _SkillInfo;
 
     enum en_SkillExplanationImage
     {
@@ -18,7 +18,10 @@ public class UI_SkillExplanation : UI_Base
         SkillExplanationName,
         SkillExplanationText,
         SkillExplanationCastingTime,
-        SkillExplanationCoolTime
+        SkillExplanationCoolTime,
+        SkilDamage,
+        SkilMinDamageText,
+        SkilMaxDamageText
     }
 
     public override void Init()
@@ -42,8 +45,25 @@ public class UI_SkillExplanation : UI_Base
 
         GetImage((int)en_SkillExplanationImage.SkillExplanationImage).sprite = SkillImage;
         GetTextMeshPro((int)en_SkillExplanationText.SkillExplanationName).text = _SkillInfo.SkillName;
-        GetTextMeshPro((int)en_SkillExplanationText.SkillExplanationText).text = _SkillInfo.SkillExplanation;
-        if(_SkillInfo.SkillCastingTime == 0)
+        GetTextMeshPro((int)en_SkillExplanationText.SkillExplanationText).text = Managers.String._SkillExplanationString[_SkillInfo.SkillType];
+
+        switch(SkillInfo.SkillType)
+        {
+            case en_SkillType.SKILL_DEFAULT_ATTACK:
+            case en_SkillType.SKILL_PUBLIC_ACTIVE_BUF_SHOCK_RELEASE:
+            case en_SkillType.SKILL_FIGHT_ACTIVE_BUF_CHARGE_POSE:
+                GetTextMeshPro((int)en_SkillExplanationText.SkilDamage).text = "";
+                GetTextMeshPro((int)en_SkillExplanationText.SkilMinDamageText).text = "";
+                GetTextMeshPro((int)en_SkillExplanationText.SkilMaxDamageText).text = "";
+                break;
+            default:
+                GetTextMeshPro((int)en_SkillExplanationText.SkilDamage).text = "피해량";
+                GetTextMeshPro((int)en_SkillExplanationText.SkilMinDamageText).text = SkillInfo.SkillMinDamage.ToString() + " ~ ";
+                GetTextMeshPro((int)en_SkillExplanationText.SkilMaxDamageText).text = SkillInfo.SkillMaxDamage.ToString();
+                break;
+        }
+
+        if (_SkillInfo.SkillCastingTime == 0)
         {
             GetTextMeshPro((int)en_SkillExplanationText.SkillExplanationCastingTime).text = "즉시 시전";
         }
@@ -51,12 +71,12 @@ public class UI_SkillExplanation : UI_Base
         {
             GetTextMeshPro((int)en_SkillExplanationText.SkillExplanationCastingTime).text = "시전 시간 : " + (_SkillInfo.SkillCastingTime / 1000.0f).ToString() + " 초";
         }
-        
+
         GetTextMeshPro((int)en_SkillExplanationText.SkillExplanationCoolTime).text = " 재사용대기 시간 : " + (_SkillInfo.SkillCoolTime / 1000.0f).ToString() + " 초";
     }
 
     public override void ShowCloseUI(bool IsShowClose)
     {
         gameObject.SetActive(IsShowClose);
-    }    
+    }
 }
