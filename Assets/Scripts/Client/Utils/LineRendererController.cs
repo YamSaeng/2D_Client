ac9b290d222d15ee7b3ae745bba0317cb07e2857
정성원 <tyhg56@naver.com>
2023-04-20 14:00:56 +0900

    Fix : " LineRendererController 수정 "
    LineRendererController
     - _Direction 추가 ( 충돌체 방향 )
     - _Position 추가 ( 충돌체 기준 좌표 )
     - _MiddlePosition 추가 ( 충돌체 중심 좌표 )
     - _LeftTop 추가 ( 사각 충돌체 왼쪽 위 좌표 )
       _LeftDown 추가 ( 사각 충돌체 왼쪽 아래 좌표 )
       _RightTop 추가 ( 사각 충돌체 오른쪽 위 좌표 )
       _RightDown 추가 ( 사각 충돌체 오른쪽 아래 좌표 )
     - _Size 추가 ( 충돌체 크기 )
     - SetUpOwnPlayer() 수정
      - _Direction 저장 추가
     - SetDirection() 추가 ( _Direction 저장 )
     - PositionUpdate() 추가
      - _Position 좌표 기준으로
           LeftTop, LeftDown, RightTop, RightDown 좌표 계산
           MiddlePosition 좌표 계산
     - RotateUpdate() 추가
      - _Direction ( 방향 값 ) 기준으로 사각 충돌체 회전
     - UpdateMainRectCollision() 수정
      - 좌표, 회전 업데이트

--- Assets/Scripts/Client/Utils/LineRendererController.cs
+++ Assets/Scripts/Client/Utils/LineRendererController.cs
@@ -5,19 +5,39 @@ using UnityEngine;
 [RequireComponent(typeof(LineRenderer))]
 public class LineRendererController : MonoBehaviour
 {
-    private CreatureObject _OwnCreature;
+    private CBaseObject _OwnCreature;
     private LineRenderer _LineRenderer;
     private List<Vector3> _MainRectCollision = new List<Vector3>();
     private List<st_RayCastingPosition> _RayCastingPositions = new List<st_RayCastingPosition>();
+    
+    private Vector2 _Direction;
+
+    private Vector2 _Position;
+
+    private Vector2 _MiddlePosition;
+            
+    private Vector2 _LeftTop;
+    private Vector2 _LeftDown;
+    private Vector2 _RightTop;
+    private Vector2 _RightDown;
+    private Vector2 _LeftDownToTop;    
+
+    private Vector2 _Size;
 
     private void Awake()
     {
         _LineRenderer = GetComponent<LineRenderer>();
     }
 
-    public void SetUpOwnPlayer(CreatureObject OwnPlayer)
+    public void SetUpOwnPlayer(CBaseObject OwnPlayer)
     {
         _OwnCreature = OwnPlayer;
+        _Direction = _OwnCreature._GameObjectInfo.ObjectPositionInfo.LookAtDireciton;
+    }
+
+    public void SetDirection(Vector2 Direction)
+    {
+        _Direction = Direction;
     }
 
     public void SetRayCastingPositions(st_RayCastingPosition[] RayCastingPositions)
@@ -64,39 +84,82 @@ public class LineRendererController : MonoBehaviour
     {
         if (_OwnCreature != null && _LineRenderer != null)
         {   
-            Vector3 OwnPlayerPosition = _OwnCreature.transform.position;
-            Vector2 Size = new Vector2();            
+            Vector3 OwnPlayerPosition = _OwnCreature.transform.position;            
 
             switch (_OwnCreature._GameObjectInfo.ObjectType)
             {
                 case en_GameObjectType.OBJECT_PLAYER:
                 case en_GameObjectType.OBJECT_GOBLIN:
                 case en_GameObjectType.OBJECT_ARCHITECTURE_CRAFTING_TABLE_SAWMILL:
-                    Size.x = 1.0f;
-                    Size.y = 1.0f;
+                    _Size.x = 1.0f;
+                    _Size.y = 1.0f;
                     break;
                 case en_GameObjectType.OBJECT_ARCHITECTURE_CRAFTING_TABLE_FURNACE:
-                    Size.x = 2.0f;
-                    Size.y = 2.0f;
+                    _Size.x = 2.0f;
+                    _Size.y = 2.0f;  
+                    break;
+                case en_GameObjectType.OBJECT_SKILL_SWORD_BLADE:
+                    _Size.x = 1.0f;
+                    _Size.y = 0.5f;
                     break;
             }
 
-            Vector3 LeftTop = new Vector3(OwnPlayerPosition.x, OwnPlayerPosition.y);
-            Vector3 RightTop = new Vector3(OwnPlayerPosition.x + Size.x, OwnPlayerPosition.y);
-            Vector3 RightDown = new Vector3(OwnPlayerPosition.x + Size.x, OwnPlayerPosition.y - Size.y);
-            Vector3 LeftDown = new Vector3(OwnPlayerPosition.x, OwnPlayerPosition.y - Size.y);
-            Vector3 LeftDownToTop = new Vector3(OwnPlayerPosition.x, OwnPlayerPosition.y);
-
-            _MainRectCollision.Add(LeftTop);
-            _MainRectCollision.Add(RightTop);
-            _MainRectCollision.Add(RightDown);
-            _MainRectCollision.Add(LeftDown);
-            _MainRectCollision.Add(LeftDownToTop);
+            PositionUpdate();
+            RotateUpdate();
+          
+            _MainRectCollision.Add(_LeftTop);
+            _MainRectCollision.Add(_RightTop);
+            _MainRectCollision.Add(_RightDown);
+            _MainRectCollision.Add(_LeftDown);
+            _MainRectCollision.Add(_LeftDownToTop);
 
             _LineRenderer.positionCount = _MainRectCollision.Count;
             _LineRenderer.startWidth = 0.04f;
             _LineRenderer.endWidth = 0.04f;
         }
+    }
+
+    private void PositionUpdate()
+    {
+        _Position = _OwnCreature.transform.position;
+
+        _MiddlePosition.x = _Position.x + _Size.x / 2.0f;
+        _MiddlePosition.y = _Position.y - _Size.y / 2.0f;
+
+        _LeftTop = _Position;
+
+        _LeftDown.x = _Position.x;
+        _LeftDown.y = _Position.y - _Size.y;
+
+        _RightTop.x = _Position.x + _Size.x;
+        _RightTop.y = _Position.y;
+
+        _RightDown.x = _Position.x + _Size.x;
+        _RightDown.y = _Position.y - _Size.y;
+
+        _LeftDownToTop = _Position;
+    }
+
+    private void RotateUpdate()
+    {
+        float Sin = Mathf.Sin(Mathf.Atan2(_Direction.y, _Direction.x));
+        float Cos = Mathf.Cos(Mathf.Atan2(_Direction.y, _Direction.x));
+
+        Vector2 Basis1 = new Vector2(Cos, Sin);
+        Vector2 Basis2 = new Vector2(-Sin, Cos);
+        Matrix2x2 RotationMatrix = new Matrix2x2(Basis1, Basis2);
+
+        Vector2 LeftTopRot = RotationMatrix * (_LeftTop - _MiddlePosition);
+        Vector2 LeftDownRot = RotationMatrix * (_LeftDown - _MiddlePosition);
+        Vector2 RightTopRot = RotationMatrix * (_RightTop - _MiddlePosition);
+        Vector2 RightDownRot = RotationMatrix * (_RightDown - _MiddlePosition);
+        Vector2 LeftDownToTopRotate = RotationMatrix * (_LeftTop - _MiddlePosition);
+
+        _LeftTop = LeftTopRot + _MiddlePosition;
+        _LeftDown = LeftDownRot + _MiddlePosition;
+        _RightTop = RightTopRot + _MiddlePosition;
+        _RightDown = RightDownRot + _MiddlePosition;
+        _LeftDownToTop = LeftDownToTopRotate + _MiddlePosition;        
     }
 
     public void UpdateRayCasting()
