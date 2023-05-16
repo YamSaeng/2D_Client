658c9ebbc706c3dd39ebe8ed85ebc44bbcc44c64
정성원 <tyhg56@naver.com>
2023-05-16 15:28:15 +0900

    Fix : " UI_OptionItem 수정 "
    UI_OptionItem
     - OnOptionItemClick() 수정
      - MENU_TYPE_CHARACTER_CHOICE 처리 추가 ( 캐릭터 선택 창으로 이동 )

--- Assets/Scripts/Client/UI/Option/UI_OptionItem.cs
+++ Assets/Scripts/Client/UI/Option/UI_OptionItem.cs
@@ -7,7 +7,7 @@ using UnityEngine.EventSystems;
 
 public class UI_OptionItem : UI_Base
 {
-    en_OptionType _OptionType;
+    en_MenuType _OptionType;
 
     enum en_OptionItemGameObject
     {
@@ -36,23 +36,38 @@ public class UI_OptionItem : UI_Base
         gameObject.SetActive(IsShowClose);
     }
 
-    public void SetOptionItemType(en_OptionType OptionType)
+    public void SetOptionItemType(en_MenuType OptionType)
     {
         _OptionType = OptionType;
+
+        switch(OptionType)
+        {
+            case en_MenuType.MENU_TYPE_CHARACTER_CHOICE:
+                GetTextMeshPro((int)en_OptionItemText.UI_OptionItemText).text = "캐릭터 선택";
+                break;
+        }
     }
 
     private void OnOptionItemClick(PointerEventData Event)
     {
         UI_GameScene GameSceneUI = Managers.UI._SceneUI as UI_GameScene;
         if (GameSceneUI != null)
-        {
+        {            
             switch (_OptionType)
-            {
-                case en_OptionType.OPTION_TYPE_NONE:                  
+            {                
+                case en_MenuType.MENU_TYPE_CHARACTER_CHOICE:                    
+
+                    CMessage ReqMenuOptionPacket = Packet.MakePacket.ReqMakeMenuOptionPacket(_OptionType);
+                    Managers.NetworkManager.GameServerSend(ReqMenuOptionPacket);
+
+                    Managers.NetworkManager._ServerState = en_ServerState.SERVER_STATE_LOGIN_CHARACTER_CHOICE;
+
+                    // LoginScene 로딩
+                    Managers.Scene.LoadScene(Define.en_Scene.LoginScene);                    
                     break;
-            }           
+            }            
 
             GameSceneUI.DeleteGameSceneUIStack(GameSceneUI._OptionUI);
         }
-    }
+    }    
 }
