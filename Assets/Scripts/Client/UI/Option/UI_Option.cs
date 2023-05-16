81052e3f362cdf4e9488157334b7484f6943b175
정성원 <tyhg56@naver.com>
2023-05-16 16:36:33 +0900

    Fix : " UI_Option 수정 "
    UI_Option
     - Binding() 수정
      - 메뉴 캐릭터 선택 창 추가

--- Assets/Scripts/Client/UI/Option/UI_Option.cs
+++ Assets/Scripts/Client/UI/Option/UI_Option.cs
@@ -5,7 +5,7 @@ using UnityEngine.EventSystems;
 
 public class UI_Option : UI_Base
 {
-    Dictionary<en_OptionType, UI_OptionItem> _OptionItems;
+    Dictionary<en_MenuType, UI_OptionItem> _OptionItems;
 
     enum en_OptionGameObject
     {
@@ -22,17 +22,17 @@ public class UI_Option : UI_Base
     {
         Bind<GameObject>(typeof(en_OptionGameObject));
 
-        //BindEvent(GetGameObject((int)en_OptionGameObject.UI_Option).gameObject, OnOptionBoxDrag , Define.en_UIEvent.Drag);
+        BindEvent(GetGameObject((int)en_OptionGameObject.UI_Option).gameObject, OnOptionBoxDrag , Define.en_UIEvent.Drag);
 
         gameObject.SetActive(false);
 
         RectTransform OptionUIRectTransform = GetComponent<RectTransform>();
         OptionUIRectTransform.localPosition = new Vector3(0, 200.0f, 0);
 
-        //GameObject OptionItemGO = Managers.Resource.Instantiate(en_ResourceName.CLIENT_UI_NAME_OPTION, GetGameObject((int)en_OptionGameObject.OptionItemList).gameObject.transform);
-        //UI_OptionItem OptionItemUI = OptionItemGO.GetComponent<UI_OptionItem>();
-        //OptionItemUI.SetOptionItemType(en_OptionType.OPTION_TYPE_TILE_BUY);
-        //OptionItemUI.Binding();
+        GameObject OptionItemGO = Managers.Resource.Instantiate(en_ResourceName.CLIENT_UI_OPTION_ITEM, GetGameObject((int)en_OptionGameObject.OptionItemList).gameObject.transform);
+        UI_OptionItem OptionItemUI = OptionItemGO.GetComponent<UI_OptionItem>();
+        OptionItemUI.Binding();
+        OptionItemUI.SetOptionItemType(en_MenuType.MENU_TYPE_CHARACTER_CHOICE);
     }
 
     public override void ShowCloseUI(bool IsShowClose)
