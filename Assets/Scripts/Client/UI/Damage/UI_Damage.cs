f227f95b8f84bd1a260250dfd35ff19f6b2fadb3
정성원 <tyhg56@naver.com>
2023-07-28 11:12:02 +0900

    Fix : " UI_Damage 수정 "
    UI_Damage
     - Setup() 수정
      - en_SkillType -> en_SkillKinds 변경
      - SkillKind에 따라 처리

--- Assets/Scripts/Client/UI/Damage/UI_Damage.cs
+++ Assets/Scripts/Client/UI/Damage/UI_Damage.cs
@@ -17,7 +17,7 @@ public class UI_Damage : MonoBehaviour
         _TextMesh = transform.GetComponent<TextMeshPro>();
     }
 
-    public void Setup(en_SkillType SkillType, int PointAmount, bool IsCriticalHit, float PositionX, float PositionY)
+    public void Setup(en_SkillKinds SkillKind, int PointAmount, bool IsCriticalHit, float PositionX, float PositionY)
     {
         GameObject UIRoot = GameObject.Find("@UI_Root");
         if (UIRoot == null)
@@ -27,47 +27,17 @@ public class UI_Damage : MonoBehaviour
 
         transform.SetParent(UIRoot.transform);
 
-        switch (SkillType)
+        switch (SkillKind)
         {
-            case en_SkillType.SKILL_DEFAULT_ATTACK:
-            case en_SkillType.SKILL_FIGHT_ACTIVE_ATTACK_FIERCE_ATTACK:
-            case en_SkillType.SKILL_FIGHT_ACTIVE_ATTACK_CONVERSION_ATTACK:
-            case en_SkillType.SKILL_FIGHT_ACTIVE_ATTACK_WRATH_ATTACK:
-            case en_SkillType.SKILL_FIGHT_ACTIVE_ATTACK_SMASH_WAVE:
-            case en_SkillType.SKILL_FIGHT_ACTIVE_ATTACK_FLY_KNIFE:
-            case en_SkillType.SKILL_FIGHT_ACTIVE_ATTACK_COMBO_FLY_KNIFE:
-            case en_SkillType.SKILL_FIGHT_ACTIVE_ATTACK_JUMPING_ATTACK:
-            case en_SkillType.SKILL_FIGHT_ACTIVE_ATTACK_PIERCING_WAVE:
-            case en_SkillType.SKILL_PROTECTION_ACTIVE_ATTACK_POWERFUL_ATTACK:
-            case en_SkillType.SKILL_PROTECTION_ACTIVE_ATTACK_SHARP_ATTACK:
-            case en_SkillType.SKILL_PROTECTION_ACTIVE_ATTACK_LAST_ATTACK:
-            case en_SkillType.SKILL_PROTECTION_ACTIVE_ATTACK_SHIELD_SMASH:
-            case en_SkillType.SKILL_PROTECTION_ACTIVE_ATTACK_SHIELD_COUNTER:
-            case en_SkillType.SKILL_PROTECTION_ACTIVE_ATTACK_SWORD_STORM:
-            case en_SkillType.SKILL_PROTECTION_ACTIVE_ATTACK_CAPTURE:
-            case en_SkillType.SKILL_ASSASSINATION_ACTIVE_ATTACK_QUICK_CUT:
-            case en_SkillType.SKILL_ASSASSINATION_ACTIVE_ATTACK_FAST_CUT:
-            case en_SkillType.SKILL_ASSASSINATION_ACTIVE_ATTACK_BACK_ATTACK:
-            case en_SkillType.SKILL_ASSASSINATION_ACTIVE_ATTACK_BACK_CUT:
-            case en_SkillType.SKILL_ASSASSINATION_ACTIVE_ATTACK_ADVANCE_CUT:
-            case en_SkillType.SKILL_ASSASSINATION_ACTIVE_ATTACK_POISON_INJECTION:
-            case en_SkillType.SKILL_ASSASSINATION_ACTIVE_ATTACK_POISON_STUN:
-            case en_SkillType.SKILL_ASSASSINATION_ACTIVE_ATTACK_ASSASSINATION:
-            case en_SkillType.SKILL_ASSASSINATION_ACTIVE_ATTACK_BACK_STEP:
-            case en_SkillType.SKILL_SHOOTING_ACTIVE_ATTACK_SNIFING:            
-            case en_SkillType.SKILL_SPELL_ACTIVE_ATTACK_FLAME_BOLT:
-            case en_SkillType.SKILL_SPELL_ACTIVE_ATTACK_ICE_CHAIN:
-            case en_SkillType.SKILL_SPELL_ACTIVE_ATTACK_ICE_WAVE:
-            case en_SkillType.SKILL_SPELL_ACTIVE_ATTACK_LIGHTNING_STRIKE:
-            case en_SkillType.SKILL_SPELL_ACTIVE_ATTACK_HEL_FIRE:
-            case en_SkillType.SKILL_DISCIPLINE_ACTIVE_ATTACK_DIVINE_STRIKE:
-            case en_SkillType.SKILL_GOBLIN_ACTIVE_MELEE_DEFAULT_ATTACK:            
+            case en_SkillKinds.SKILL_KIND_MELEE_SKILL:
+            case en_SkillKinds.SKILL_KIND_SPELL_SKILL:
+            case en_SkillKinds.SKILL_KIND_RANGE_SKILL:
                 // 크리티컬 여부에 따라 크기와 색 조정
                 if (IsCriticalHit == false)
                 {
                     // 데미지 텍스트 적용
                     _TextMesh.SetText(PointAmount.ToString());
-                    _TextMesh.fontSize = 5;                    
+                    _TextMesh.fontSize = 5;
 
                     _TextMesh.color = DefaultColor;
                 }
@@ -75,11 +45,10 @@ public class UI_Damage : MonoBehaviour
                 {
                     _TextMesh.SetText("치명타! " + PointAmount.ToString());
                     _TextMesh.fontSize = 8;
-                    _TextMesh.color = new Color(200,0,0);
+                    _TextMesh.color = new Color(200, 0, 0);
                 }
                 break;
-            case en_SkillType.SKILL_DISCIPLINE_ACTIVE_HEAL_HEALING_LIGHT:
-            case en_SkillType.SKILL_DISCIPLINE_ACTIVE_HEAL_HEALING_WIND:
+            case en_SkillKinds.SKILL_KIND_HEAL_SKILL:
                 // 크리티컬 여부에 따라 크기와 색 조정
                 if (IsCriticalHit == false)
                 {
@@ -95,7 +64,7 @@ public class UI_Damage : MonoBehaviour
                     _TextMesh.fontSize = 8;
                     _TextMesh.color = HealColor;
                 }
-                break;            
+                break;                
         }
 
         // 뒤에 나타난 데미지 팝업 ui가 앞쪽에 출력 될 수 있도록하기 위해 _SortingOrder 증가
