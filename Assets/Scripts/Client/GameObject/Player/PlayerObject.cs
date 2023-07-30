dbc7a731206b15ba4322f06af95ed3d0862cd5f6
정성원 <tyhg56@naver.com>
2023-07-30 12:15:48 +0900

    Fix : " PlayerObject 수정 "
    PlayerObject
     - SearchAroundObject() 추가
      - 일정 거리 안에 있는 오브젝트 탐색해서 저장
     - FindAroundObject() 추가
      - 저장해둔 오브젝트를 가져와서 거리가 가까운 대상으로 반환
      - 대상 정보 서버에 요청

--- Assets/Scripts/Client/GameObject/Player/PlayerObject.cs
+++ Assets/Scripts/Client/GameObject/Player/PlayerObject.cs
@@ -1,6 +1,8 @@
 ﻿using ServerCore;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
@@ -8,7 +10,25 @@ using UnityEngine.UI;
 using static Define;
 
 public class PlayerObject : CreatureObject
-{
+{    
+    Dictionary<long, CreatureObject> _AroundObjects = new Dictionary<long, CreatureObject>();
+    List<st_AroundObject> _SelectedObjects = new List<st_AroundObject>();
+
+    public struct st_AroundObject : IComparable<st_AroundObject>
+    {
+        public long ObjectID;        
+        public float Distance;      
+
+        public int CompareTo(st_AroundObject Other)
+        {
+            return Distance < Other.Distance ? 1 : 0;
+        }      
+    }
+        
+    PriorityQueue<st_AroundObject> _DistanceAroundObjects = new PriorityQueue<st_AroundObject>();
+
+    int _SelectdObjectCount = 0;
+
     public override void Init()
     {
         base.Init();        
@@ -36,6 +56,102 @@ public class PlayerObject : CreatureObject
 
     protected override void UpdateController()
     {
-        base.UpdateController();        
-    }         
+        base.UpdateController();
+
+        SearchAroundObject();
+    }
+
+    public void SearchAroundObject()
+    {
+        if (_GameObjectInfo.ObjectId == Managers.NetworkManager._PlayerDBId)
+        {
+            List<GameObject> AroundObjects = Managers.Object.GetObjects();
+            if (AroundObjects.Count > 0)
+            {
+                _AroundObjects.Clear();
+
+                foreach (GameObject AroundObject in AroundObjects)
+                {
+                    CreatureObject Object = AroundObject?.GetComponent<CreatureObject>();
+                    if (Object != null)
+                    {
+                        if(Object._GameObjectInfo.ObjectId == _GameObjectInfo.ObjectId)
+                        {
+                            continue;
+                        }    
+
+                        float Distance = Vector2.Distance(AroundObject.transform.position, transform.position);
+                        if (Distance < 6.0f)
+                        {
+                            _AroundObjects.Add(Object._GameObjectInfo.ObjectId, Object);
+                        }
+                    }   
+                }
+            }
+        }
+    }
+
+    public void FindAroundObject()
+    {
+        if(_AroundObjects.Count == 0)
+        {
+            return;
+        }
+
+        _DistanceAroundObjects.Clear();
+                
+        foreach (CreatureObject AroundObject in _AroundObjects.Values.ToList())
+        {
+            bool IsSelectObject = false;
+
+            float Distance = Vector2.Distance(AroundObject.transform.position, transform.position);
+
+            st_AroundObject STAroundObject;
+            STAroundObject.ObjectID = AroundObject._GameObjectInfo.ObjectId;            
+            STAroundObject.Distance = Distance;
+
+            // 선택했던 대상들에 있는지 확인
+            foreach(st_AroundObject SelectObject in _SelectedObjects)
+            {
+                if(SelectObject.ObjectID == STAroundObject.ObjectID)
+                {
+                    IsSelectObject = true;
+                }
+            }
+
+            if(!IsSelectObject)
+            {
+                _DistanceAroundObjects.Push(STAroundObject);
+            }            
+        }
+
+        if(_DistanceAroundObjects.Count() > 0)
+        {
+            st_AroundObject AroundObjectData= _DistanceAroundObjects.Pop();         
+
+            GameObject AroundObjectGO = Managers.Object.FindById(AroundObjectData.ObjectID);
+            if(AroundObjectGO != null)
+            {
+                CreatureObject AroundCreatureObject = AroundObjectGO.GetComponent<CreatureObject>();
+                if(AroundCreatureObject != null)
+                {
+                    _SelectdObjectCount++;
+
+                    _SelectedObjects.Add(AroundObjectData);
+
+                    if(_AroundObjects.Count() == _SelectdObjectCount)
+                    {
+                        _SelectedObjects.Clear();
+
+                        _SelectdObjectCount = 0;
+                    }
+
+                    CMessage ReqLeftMousePositionObjectInfoPacket = Packet.MakePacket.ReqMakeLeftMouseWorldObjectInfoPacket(
+                                   AroundCreatureObject._GameObjectInfo.ObjectId,
+                                   AroundCreatureObject._GameObjectInfo.ObjectType);
+                    Managers.NetworkManager.GameServerSend(ReqLeftMousePositionObjectInfoPacket);
+                }
+            }
+        }                
+    }
 }
