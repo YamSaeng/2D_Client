aa204a08fc758ae7c80adfc51a83f27eae05bd79
정성원 <tyhg56@naver.com>
2023-08-20 16:24:04 +0900

    Docs : " Protocol 수정 "
    Protocol
     - en_PACKET_C2S_LEFT_DRAG_OBJECTS_SELECT 추가 ( 게임서버 왼쪽 마우스 드래그 다중 선택 요청 )
     - en_PACKET_S2C_LEFT_DRAG_OBJECTS_SELECT 추가  ( 게임서버 왼쪽 마우스 드래그 다중 선택 요청 응답 )

--- Assets/Scripts/Server/Protocol.cs
+++ Assets/Scripts/Server/Protocol.cs
@@ -318,6 +318,23 @@ public class Protocol
 		en_PACKET_S2C_LEFT_MOUSE_OBJECT_INFO,
 
 		//------------------------------------------------------------
+		// 게임서버 왼쪽 마우스 드래그 다중 선택 요청
+		// int64 AccountID
+		// int64 PlayerID
+		// float MouseDragStartPositionX
+		// float MouseDragStartPositionY
+		// float MouseDragEndPositionX
+		// float MouseDragEndPositionY
+		//------------------------------------------------------------
+		en_PACKET_C2S_LEFT_DRAG_OBJECTS_SELECT,
+
+		//------------------------------------------------------------
+		// 게임서버 왼쪽 마우스 드래그 다중 선택 요청 응답
+		// long ObjectIds
+		//------------------------------------------------------------
+		en_PACKET_S2C_LEFT_DRAG_OBJECTS_SELECT,
+
+		//------------------------------------------------------------
 		// 게임서버 왼쪽 UI 마우스 정보 요청 
 		// int64 AccountID
 		// int64 PlayerID
