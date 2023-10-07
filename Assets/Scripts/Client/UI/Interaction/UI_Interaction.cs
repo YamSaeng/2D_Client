eb0efcf8d7e9f486fee41ec131db779b9b301244
정성원 <tyhg56@naver.com>
2023-10-07 19:36:07 +0900

    Fix : " UI_Interaction 수정 "
    UI_Interaction
     - _ObjectType 추가 ( 상호 작용 대상  ObjectType )
     - _InteractionType 추가 ( 상호 작용 Type )
     - C2S_InteractionPacketSend() 추가
      - 상호작용 서버로 전송

--- Assets/Scripts/Client/UI/Interaction/UI_Interaction.cs
+++ Assets/Scripts/Client/UI/Interaction/UI_Interaction.cs
@@ -1,3 +1,4 @@
+using ServerCore;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -5,7 +6,9 @@ using UnityEngine;
 
 public class UI_Interaction : UI_Base
 {
+    private en_GameObjectType _ObjectType;
     private long _ObjectID;
+    private en_InteractionType _InteractionType;
 
     enum en_InteractionText
     {
@@ -40,15 +43,18 @@ public class UI_Interaction : UI_Base
         GetGameObject((int)en_InteractionGameObject.UI_InteractionGameObject).gameObject.SetActive(IsShowClose);        
     }
 
-    public void SetInteractionInfoText(en_GameObjectType GameObjectType, long ObjectID)
+    public void SetInteractionInfoText(en_GameObjectType ObjectType, long ObjectID)
     {
+        _ObjectType = ObjectType;
         _ObjectID = ObjectID;
 
         string InteractionInfoString = "";
 
-        switch(GameObjectType)
+        switch(ObjectType)
         {
             case en_GameObjectType.OBJECT_GOBLIN:
+                _InteractionType = en_InteractionType.INTERACTION_TYPE_ROOTING;
+
                 InteractionInfoString = "고블린 시체";
                 break;
         }
@@ -61,6 +67,12 @@ public class UI_Interaction : UI_Base
     public void SetInteractionKey(en_KeyCode KeyCode)
     {
         GetTextMeshPro((int)en_InteractionText.InteractionKeyText).text = Managers.String._KeyCodeString[KeyCode];
+    }
+
+    public void C2S_InteractionPacketSend()
+    {
+        CMessage ReqInteractionPacket = Packet.MakePacket.ReqMakeInteractionPacket(_InteractionType, _ObjectType, _ObjectID);
+        Managers.NetworkManager.GameServerSend(ReqInteractionPacket);
     }
 
     private void Update()
