0207d411055e88ec5085b41d30a6a9513e28508d
정성원 <tyhg56@naver.com>
2023-10-11 14:10:04 +0900

    Fix : " UI_Builiding 수정 "
    UI_Builiding
     - _BuildingMainHall 추가 ( 관청 건축물 )
     - _BuilidingWeaponStore 추가 ( 무기 상점 건축물 )
     - _BuildingArmorStore 추가 ( 갑옷 상점 건축물 )
     - en_BuildingGameObject 수정
      - DummyBuilding 추가
     - en_BuildingImage 추가
      - DummyBuildingImage 추가
     - Binding() 수정
      - Image 바인딩 추가
      - 관청, 무기상점, 갑옷상점 데이터 설정

--- Assets/Scripts/Client/UI/Building/UI_Building.cs
+++ Assets/Scripts/Client/UI/Building/UI_Building.cs
@@ -17,6 +17,10 @@ public class UI_Building : UI_Base
     [HideInInspector]
     public byte ArmorBuildingCount;
 
+    private UI_BuildingItem _BuildingMainHall;
+    private UI_BuildingItem _BuildingWeaponStore;
+    private UI_BuildingItem _BuildingArmorStore;
+
     enum en_BuildingButton
     {
         GovernmentOfficeListButton,        
@@ -28,7 +32,13 @@ public class UI_Building : UI_Base
     {
         BuildingGovermentOffice,
         BuildingWeaponStore,
-        BuildingArmorStore
+        BuildingArmorStore,
+        DummyBuilding
+    }
+
+    enum en_BuildingImage
+    {
+        DummyBuildingImage
     }
 
     enum en_BuildingText
@@ -49,8 +59,9 @@ public class UI_Building : UI_Base
     public override void Binding()
     {
         Bind<Button>(typeof(en_BuildingButton));
-        Bind<GameObject>(typeof(en_BuildingGameObject));        
-        Bind<TextMeshProUGUI>(typeof(en_BuildingText));
+        Bind<GameObject>(typeof(en_BuildingGameObject));
+        Bind<Image>(typeof(en_BuildingImage));
+        Bind<TextMeshProUGUI>(typeof(en_BuildingText));        
 
         BindEvent(GetButton((int)en_BuildingButton.GovernmentOfficeListButton).gameObject, OnGovermentOfficeListButtonClick, Define.en_UIEvent.MouseClick);
         BindEvent(GetButton((int)en_BuildingButton.StoreListButton).gameObject, OnStoreListButtonClick, Define.en_UIEvent.MouseClick);
@@ -59,6 +70,42 @@ public class UI_Building : UI_Base
         BindEvent(GetGameObject((int)en_BuildingGameObject.BuildingGovermentOffice).gameObject, OnMainHallBuildingButtonClick, Define.en_UIEvent.MouseClick);
         BindEvent(GetGameObject((int)en_BuildingGameObject.BuildingWeaponStore).gameObject, OnWeaponStoreBuildingButtonClick, Define.en_UIEvent.MouseClick);
         BindEvent(GetGameObject((int)en_BuildingGameObject.BuildingArmorStore).gameObject, OnArmorStoreBuildingButtonClick, Define.en_UIEvent.MouseClick);
+
+        _BuildingMainHall = GetGameObject((int)en_BuildingGameObject.BuildingGovermentOffice)?.GetComponent<UI_BuildingItem>();
+        if(_BuildingMainHall != null)
+        {
+            st_BuildingInfo MainHallInfo = new st_BuildingInfo();
+            MainHallInfo.BuildingHeight = 4;
+            MainHallInfo.BuildingWidth = 4;
+            MainHallInfo.BuildinSmallCategory = en_BuildingSmallCategory.BUILDING_SMALL_CATEGORY_MAIN_HALL;            
+
+            _BuildingMainHall.SetBuildingInfo(MainHallInfo);
+            _BuildingMainHall.ShowCloseUI(false);
+        }
+
+        _BuildingWeaponStore = GetGameObject((int)en_BuildingGameObject.BuildingWeaponStore)?.GetComponent<UI_BuildingItem>();
+        if (_BuildingWeaponStore != null)
+        {
+            st_BuildingInfo WeaponStoreInfo = new st_BuildingInfo();
+            WeaponStoreInfo.BuildingHeight = 2;
+            WeaponStoreInfo.BuildingWidth = 2;
+            WeaponStoreInfo.BuildinSmallCategory = en_BuildingSmallCategory.BUILDING_SMALL_CATEGORY_WEAPON_STORE;
+
+            _BuildingWeaponStore.SetBuildingInfo(WeaponStoreInfo);
+            _BuildingWeaponStore.ShowCloseUI(false);
+        }
+
+        _BuildingArmorStore = GetGameObject((int)en_BuildingGameObject.BuildingArmorStore)?.GetComponent<UI_BuildingItem>();
+        if (_BuildingArmorStore != null)
+        {
+            st_BuildingInfo ArmorStoreInfo = new st_BuildingInfo();
+            ArmorStoreInfo.BuildingHeight = 2;
+            ArmorStoreInfo.BuildingWidth = 2;
+            ArmorStoreInfo.BuildinSmallCategory = en_BuildingSmallCategory.BUILDING_SMALL_CATEGORY_ARMOR_STORE;
+
+            _BuildingArmorStore.SetBuildingInfo(ArmorStoreInfo);
+            _BuildingArmorStore.ShowCloseUI(false);
+        }
 
         GetGameObject((int)en_BuildingGameObject.BuildingGovermentOffice)?.GetComponent<UI_BuildingItem>()?.ShowCloseUI(false);
         GetGameObject((int)en_BuildingGameObject.BuildingWeaponStore)?.GetComponent<UI_BuildingItem>()?.ShowCloseUI(false);
@@ -88,7 +135,7 @@ public class UI_Building : UI_Base
             Managers.GameMessage._GlobalMessageBox.NewStatusAbnormalMessage(en_GlobalMessageType.PERSONAL_MESSAGE_BUILDING_FAIL, "해당 건물을 더 이상 지을 수 없습니다");
             return;
         }
-        
+                
         MainHallBuildingCount--;
 
         if (MainHallBuildingCount < 0)
